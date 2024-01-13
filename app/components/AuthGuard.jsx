c0072915a18c2016cec3c23e9359558b28643c22
Waweru <ngashalex8@gmail.com>
2024-01-13 23:44:04 +0300

    Linters errors

--- app/components/AuthGuard.jsx
+++ app/components/AuthGuard.jsx
@@ -1,32 +1,32 @@
-// components/AuthGuard.js
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import { SessionProvider } from 'next-auth/react';
+// // components/AuthGuard.js
+// import { useSession } from 'next-auth/react';
+// import { useRouter } from 'next/navigation';
+// import { SessionProvider } from 'next-auth/react';
 
-const AuthGuard = (WrappedComponent) => {
-  const WithAuthGuard = (props) => {
-    const { data: session, status } = useSession();
-    const router = useRouter();
+// const AuthGuard = (WrappedComponent) => {
+//   const WithAuthGuard = (props) => {
+//     const { data: session, status } = useSession();
+//     const router = useRouter();
 
-    // If the user is not authenticated, redirect to the login page
-    if (status === 'loading') {
-      return <div>Loading...</div>;
-    }
+//     // If the user is not authenticated, redirect to the login page
+//     if (status === 'loading') {
+//       return <div>Loading...</div>;
+//     }
 
-    if (!session) {
-      router.push('/login');
-      return null;
-    }
+//     if (!session) {
+//       router.push('/login');
+//       return null;
+//     }
 
-    // If the user is authenticated, render the wrapped component
-    return <WrappedComponent {...props} />;
-  };
+//     // If the user is authenticated, render the wrapped component
+//     return <WrappedComponent {...props} />;
+//   };
 
-  return () => (
-    <SessionProvider>
-      <WithAuthGuard />
-    </SessionProvider>
-  );
-};
+//   return () => (
+//     <SessionProvider>
+//       <WithAuthGuard />
+//     </SessionProvider>
+//   );
+// };
 
-export default AuthGuard;
+// export default AuthGuard;
--- app/todo/page.js
+++ app/todo/page.js
@@ -1,9 +1,8 @@
 "use client";
 import TodoListItems from "../components/TodoListItems";
-import AuthGuard from '../components/AuthGuard';
 
 const TodoPage = () => {
   return <TodoListItems />;
 };
 
-export default AuthGuard(TodoPage);
+export default TodoPage;
